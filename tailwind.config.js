6312a8a068c87355bd7ca7b48eab5f9f5832084b
Merta Yasa <made.b4li@gmail.com>
2021-06-06 15:50:12 +0800

    Purge css

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,14 @@
 const defaultTheme = require('tailwindcss/defaultTheme');
 
 module.exports = {
-    purge: [
-        './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
-        './storage/framework/views/*.php',
-        './resources/views/**/*.blade.php',
-    ],
+    purge: {
+        enabled: true,
+        content: [
+            './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
+            './storage/framework/views/*.php',
+            './resources/views/**/*.blade.php',
+        ]
+    },
 
     theme: {
         extend: {
